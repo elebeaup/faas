15ce0c1e5715f5a81ae434013d0dfaa63d3b52de
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-11-01 12:54:17 +0000

    Extract scaling from zero
    - extracting this package means it can be used in other components
    such as the asynchronous nats-queue-worker which may need to
    invoke functions which are scaled down to zero replicas.
    
    Ref: https://github.com/openfaas/nats-queue-worker/issues/32
    
    Tested on Docker Swarm for scaling up, already scaled and not
    found error.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- gateway/scaling/function_scaler.go
+++ gateway/scaling/function_scaler.go
@@ -1,37 +1,16 @@
-// Copyright (c) OpenFaaS Author(s). All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-package handlers
+package scaling
 
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"time"
-
-	"github.com/openfaas/faas/gateway/scaling"
 )
-
-// ScalingConfig for scaling behaviours
-type ScalingConfig struct {
-	// MaxPollCount attempts to query a function before giving up
-	MaxPollCount uint
-
-	// FunctionPollInterval delay or interval between polling a function's readiness status
-	FunctionPollInterval time.Duration
-
-	// CacheExpiry life-time for a cache entry before considering invalid
-	CacheExpiry time.Duration
-
-	// ServiceQuery queries available/ready replicas for function
-	ServiceQuery scaling.ServiceQuery
-}
 
 // NewFunctionScaler create a new scaler with the specified
 // ScalingConfig
 func NewFunctionScaler(config ScalingConfig) FunctionScaler {
-	cache := scaling.FunctionCache{
-		Cache:  make(map[string]*scaling.FunctionMeta),
+	cache := FunctionCache{
+		Cache:  make(map[string]*FunctionMeta),
 		Expiry: config.CacheExpiry,
 	}
 
@@ -43,7 +22,7 @@ func NewFunctionScaler(config ScalingConfig) FunctionScaler {
 
 // FunctionScaler scales from zero
 type FunctionScaler struct {
-	Cache  *scaling.FunctionCache
+	Cache  *FunctionCache
 	Config ScalingConfig
 }
 
@@ -125,46 +104,5 @@ func (f *FunctionScaler) Scale(functionName string) FunctionScaleResult {
 		Available: true,
 		Found:     true,
 		Duration:  time.Since(start),
-	}
-}
-
-// MakeScalingHandler creates handler which can scale a function from
-// zero to N replica(s). After scaling the next http.HandlerFunc will
-// be called. If the function is not ready after the configured
-// amount of attempts / queries then next will not be invoked and a status
-// will be returned to the client.
-func MakeScalingHandler(next http.HandlerFunc, config ScalingConfig) http.HandlerFunc {
-
-	scaler := NewFunctionScaler(config)
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		functionName := getServiceName(r.URL.String())
-		res := scaler.Scale(functionName)
-
-		if !res.Found {
-			errStr := fmt.Sprintf("error finding function %s: %s", functionName, res.Error.Error())
-			log.Printf("Scaling: %s", errStr)
-
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(errStr))
-			return
-		}
-
-		if res.Error != nil {
-			errStr := fmt.Sprintf("error finding function %s: %s", functionName, res.Error.Error())
-			log.Printf("Scaling: %s", errStr)
-
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(errStr))
-			return
-		}
-
-		if res.Available {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		log.Printf("[Scale] function=%s 0=>N timed-out after %f seconds", functionName, res.Duration.Seconds())
 	}
 }
